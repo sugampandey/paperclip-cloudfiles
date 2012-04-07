643efd5b36085262c4856487f03c49d38c8bbfa1
Sugam Pandey <sugam.pandey@gmail.com>
2012-04-07 08:48:48 -0700

    Commiting changes for fixing cloud_files name

--- lib/paperclip/attachment.rb
+++ lib/paperclip/attachment.rb
@@ -275,7 +275,7 @@ module Paperclip
     end
 
     def initialize_storage #:nodoc:
-      storage_class_name = @storage.to_s.capitalize
+      storage_class_name = @storage.to_s.classify
       begin
         @storage_module = Paperclip::Storage.const_get(storage_class_name)
       rescue NameError
--- lib/paperclip/storage.rb
+++ lib/paperclip/storage.rb
@@ -1,3 +1,3 @@
 require "paperclip/storage/filesystem"
 require "paperclip/storage/s3"
-require "paperclip/storage/cloud_files"
+require "paperclip/storage/cloudfile"
